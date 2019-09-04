2390dae6d4bd8ea68f6799ffe703dfe9032c3a04
Sasha <sdrozdiuk12@gmail.com>
2019-09-04 20:41:26 +0300

    Move platform realize

---
+++ New Unity Project/Assets/Scripts/MovePlatformScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatformScript : MonoBehaviour
+{
+    public GameObject FinishObject;
+    public float Speed;
+    void Update()
+    {
+        if(gameObject.transform.position.z > FinishObject.transform.position.z)
+        {
+            gameObject.transform.Translate(-Vector3.forward * Time.deltaTime * Speed);
+        }
+        else
+        {
+            Debug.Log("Destroy");
+            Destroy(gameObject);
+        }
+    }
+}

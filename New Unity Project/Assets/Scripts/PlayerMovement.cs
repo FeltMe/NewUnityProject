416c0f857e220813db09d60ebe9f3b2b8f84b4f6
Sasha <sdrozdiuk12@gmail.com>
2019-09-05 19:50:08 +0300

    Add Ball move

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,9 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController controller;
 
     public float verticalVelocity;
-    private readonly float gravity = 80.0f;
-    private readonly float JumpForce = 40.0f;
+    private readonly float gravity = 240.0f;
+    private readonly float JumpForce = 60.0f;
+    public float Speed;
 
     private void Start()
     {
@@ -16,18 +17,33 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
+        AutooJump();
+        TempMovement();
+    }
+    private void AutooJump()
+    {
         if (controller.isGrounded)
         {
             verticalVelocity = -gravity * Time.deltaTime;
-            
-                verticalVelocity = JumpForce;
-            
+            verticalVelocity = JumpForce;
         }
         else
         {
-            verticalVelocity -=gravity * Time.deltaTime;
+            verticalVelocity -= gravity * Time.deltaTime;
         }
         Vector3 moveV = new Vector3(0, verticalVelocity, 0);
         controller.Move(moveV * Time.deltaTime);
+    }
+    
+    private void TempMovement()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(-Vector3.right * Time.deltaTime * Speed);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(-Vector3.left * Time.deltaTime * Speed);
+        }
     }
 }

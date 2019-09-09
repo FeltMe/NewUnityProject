5f691b4eecd6c28968068a9c9f6d56b996614460
Sasha <41016761+FeltMe@users.noreply.github.com>
2019-09-09 03:59:10 +0300

    Fix bugs, add cam background...

---
+++ New Unity Project/Assets/Scripts/AnimatePlatform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatePlatform : MonoBehaviour
+{
+    public CharacterController controller;
+    public Animation animation;
+
+    void Update()
+    {
+        if(controller.isGrounded)
+        {
+            animation.enabled = true;
+        }
+    }
+}
